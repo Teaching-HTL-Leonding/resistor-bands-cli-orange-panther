fb2a91ba2862c05e10c5aea486b6fcee95c62f4e
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-03-16 11:46:20 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,16 +5,26 @@ if (args.Length < 1)
     return;
 }
 
-GetColors(out string col1, out string col2, out string col3, out string colTol);
-Decode4ColorBands(col1, col2, col3, colTol, out double resistorValue, out double Tolerance);
-Console.WriteLine($"The resistor value is {resistorValue:n0} Ω and the Tolerance is +/- {Tolerance}%");
+double resistorValue = 0, Tolerance = 0;
 
-void GetColors(out string col1, out string col2, out string col3, out string colTol)
+GetColors(out string col1, out string col2, out string col3, out string col4, out string colTol);
+if (args[0].Length < 16)
+{
+    Decode4ColorBands(col1, col2, col3, colTol, out resistorValue, out Tolerance);
+}
+else
+{
+    Decode5ColorBands(col1, col2, col3, col4, colTol, out resistorValue, out Tolerance);
+}
+Console.WriteLine($"The resistor value is {resistorValue:n}Ω and the Tolerance is ± {Tolerance}%");
+
+void GetColors(out string col1, out string col2, out string col3, out string col4, out string colTol)
 {
     string colorBands = args[0];
     col1 = colorBands.Substring(0, 3);
     col2 = colorBands.Substring(4, 3);
     col3 = colorBands.Substring(8, 3);
+    col4 = colorBands.Substring(12, 3);
     colTol = colorBands.Substring(colorBands.Length - 3);
 }
 
@@ -83,3 +93,14 @@ void Decode4ColorBands(string col1, string col2, string col3, string colTol, out
     resistorValue = factor1 * valCol3;
 }
 
+void Decode5ColorBands(string col1, string col2, string col3, string col4, string colTol, out double resistorValue, out double Tolerance)
+{
+    int valCol1 = ConvertColorToDigit(col1);
+    int valCol2 = ConvertColorToDigit(col2);
+    int valCol3 = ConvertColorToDigit(col3);
+    double valCol4 = GetMultiplierFromColor(col4);
+    Tolerance = GetToleranceFromColor(colTol);
+
+    double factor1 = double.Parse(valCol1.ToString() + valCol2.ToString() + valCol3.ToString());
+    resistorValue = factor1 * valCol4;
+}
